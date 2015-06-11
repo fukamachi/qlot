25c5b665f9794f1b93d51cf95459fb0d9415b8da
Eitaro Fukamachi <e.arrows@gmail.com>
2015-06-12 03:25:04 +0900

    Add qlot:bundle.

--- src/qlot.lisp
+++ src/qlot.lisp
@@ -3,11 +3,14 @@
   (:use :cl)
   (:import-from :qlot.util
                 :with-package-functions
-                :with-local-quicklisp)
+                :with-local-quicklisp
+                :pathname-in-directory-p
+                :all-required-systems)
   (:export :install
            :update
            :install-quicklisp
            :quickload
+           :bundle
            :with-local-quicklisp))
 (in-package :qlot)
 
@@ -53,3 +56,41 @@ If PATH isn't specified, this installs it to './quicklisp/'."
           do (with-local-quicklisp system-name
                (apply #'quickload system-name args))))
   systems)
+
+(defun bundle (&optional (project-dir *default-pathname-defaults*))
+  (let ((qlhome (merge-pathnames #P"quicklisp/" project-dir))
+        systems required-systems)
+    (unless (probe-file qlhome)
+      (error "~S is not ready to qlot:bundle. Try qlot:install first." project-dir))
+    (asdf::collect-sub*directories-asd-files
+     project-dir
+     :collect (lambda (asd)
+                (unless (or (pathname-in-directory-p asd qlhome)
+                            ;; KLUDGE: Ignore skeleton.asd of CL-Project
+                            (search "skeleton" (pathname-name asd)))
+                  (push asd systems)))
+     :exclude (cons "bundle-libs" asdf::*default-source-registry-exclusions*))
+    (setf required-systems
+          (delete-if (lambda (system)
+                       (member system systems :key #'pathname-name :test #'string-equal))
+                     (delete-duplicates
+                      (mapcan #'all-required-systems
+                              (mapcan #'asdf:component-sideway-dependencies
+                                      (mapcar #'asdf:find-system (mapcar #'pathname-name systems))))
+                      :test #'string-equal)))
+    (if required-systems
+        (progn
+          (format t "~&Bundle ~D ~:*system~[s~;~:;s~]:~%" (length required-systems))
+          (princ "  ")
+          (loop for i from 1
+                for (system . rest) on required-systems
+                do (princ system)
+                if (zerop (mod i 5))
+                  do (format t "~&  ")
+                else if rest
+                       do (write-char #\Space))
+          (fresh-line)
+          (with-package-functions :ql (bundle-systems)
+            (bundle-systems required-systems
+                            :to (merge-pathnames #P"bundle-libs/" project-dir))))
+        (format t "~&Nothing to bundle.~%"))))
