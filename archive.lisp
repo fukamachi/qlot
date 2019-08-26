0674220e1a0e136201245a1fac384eb4c5b4f5a0
Eitaro Fukamachi <e.arrows@gmail.com>
2019-08-27 03:17:05 +0900

    Delete dependencies on archive, salza2 and gzip-stream.

--- archive.lisp
+++ archive.lisp
@@ -1,57 +1,16 @@
 (defpackage #:qlot/archive
   (:use #:cl)
+  (:import-from #:qlot/util
+                #:with-package-functions)
   (:import-from #:uiop
-                #:pathname-parent-directory-pathname
-                #:directory-files
-                #:subdirectories)
-  (:import-from #:archive
-                #:create-tar-file
-                #:open-archive
-                #:read-entry-from-archive
-                #:extract-files-from-archive)
-  (:import-from #:salza2
-                #:gzip-file)
-  (:import-from #:gzip-stream
-                #:with-open-gzip-file)
-  (:export #:create-tarball
-           #:extract-tarball))
+                #:with-temporary-file)
+  (:export #:extract-tarball))
 (in-package #:qlot/archive)
-
-(defun create-tarball (directory destination)
-  (let ((ignore-len (length (pathname-directory (truename (uiop:pathname-parent-directory-pathname directory)))))
-        (*default-pathname-defaults* (truename (uiop:pathname-parent-directory-pathname directory)))
-        (tar-file (make-pathname
-                   :directory (pathname-directory destination)
-                   :name (pathname-name destination)))
-        (tar-gz-file destination))
-
-    (flet ((to-relative (path)
-             (make-pathname
-              :defaults path
-              :device nil
-              :directory (cons :relative
-                               (nthcdr ignore-len (pathname-directory path)))))
-           (git-dir-p (path)
-             (find ".git"
-                   (nthcdr ignore-len (pathname-directory path))
-                   :test #'string=)))
-      (archive::create-tar-file
-       tar-file
-       (mapcar #'to-relative
-               (remove-if #'git-dir-p
-                          (nconc (uiop:subdirectories directory)
-                                 (uiop:directory-files directory))))))
-
-    (salza2:gzip-file tar-file tar-gz-file)
-    (delete-file tar-file)
-    tar-gz-file))
 
 (defun extract-tarball (tarball &optional (destination *default-pathname-defaults*))
   (let ((*default-pathname-defaults* destination))
-    (with-open-gzip-file (gzip tarball)
-      (let ((archive (archive:open-archive 'archive:tar-archive gzip)))
-        (prog1
-            (merge-pathnames
-             (archive:name (archive:read-entry-from-archive archive))
-             *default-pathname-defaults*)
-          (archive::extract-files-from-archive archive))))))
+    (with-package-functions :ql-minitar (unpack-tarball contents)
+      (with-package-functions :ql-gunzipper (gunzip)
+        (uiop:with-temporary-file (:pathname tarfile :type "tar")
+          (unpack-tarball (gunzip tarball tarfile))
+          (first (contents tarfile)))))))
