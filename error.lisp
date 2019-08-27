d7d79e3438064b3ac5ef2b5f16d2852522237a88
Eitaro Fukamachi <e.arrows@gmail.com>
2019-08-28 02:29:15 +0900

    Delete files.

--- error.lisp
+++
@@ -1,9 +0,0 @@
-(defpackage #:qlot/error
-  (:use #:cl)
-  (:export #:qlot-error
-           #:qlot-qlfile-error))
-(in-package #:qlot/error)
-
-(define-condition qlot-error (simple-error) ())
-
-(define-condition qlot-qlfile-error (qlot-error) ())
