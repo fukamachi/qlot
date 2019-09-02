efd64fe8eea54ecdd1074c1497947bb527ff976c
Eitaro Fukamachi <e.arrows@gmail.com>
2019-09-02 17:15:04 +0900

    Add --debug option to enable debug logging.

--- logger.lisp
+++ logger.lisp
@@ -2,6 +2,7 @@
   (:use #:cl)
   (:export #:*logger-message-stream*
            #:*logger-debug-stream*
+           #:*debug*
            #:message
            #:debug-log))
 (in-package #:qlot/logger)
@@ -9,7 +10,10 @@
 (defvar *logger-message-stream*
   (make-synonym-stream '*standard-output*))
 
-(defvar *logger-debug-stream* nil)
+(defvar *logger-debug-stream*
+  (make-synonym-stream '*logger-message-stream*))
+
+(defvar *debug* nil)
 
 (defun message (format-control &rest format-arguments)
   (format *logger-message-stream*
@@ -17,7 +21,7 @@
           (apply #'format nil format-control format-arguments)))
 
 (defun debug-log (format-control &rest format-arguments)
-  (when *logger-debug-stream*
+  (when *debug*
     (format *logger-debug-stream*
             "~&~A~%"
             (apply #'format nil format-control format-arguments))))
