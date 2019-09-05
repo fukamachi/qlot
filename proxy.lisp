6df7fc4f0d8c45c5c531c663179f3483a58a5967
Eitaro Fukamachi <e.arrows@gmail.com>
2019-09-05 23:20:00 +0900

    Fix qlot/proxy not to redefine multiple times.

--- proxy.lisp
+++ proxy.lisp
@@ -8,19 +8,20 @@
       (uiop:getenvp "HTTP_PROXY")))
 
 #+quicklisp
-(progn
-  ;; dummy for suppress style warning
-  (defun orig-http-fetch (&rest args)
-    (declare (ignore args)))
-  (setf (symbol-function 'orig-http-fetch) (fdefinition
-                                             (find-symbol (string '#:http-fetch) '#:ql-http)))
-  ;; do not use proxy if connect localhost
-  (setf (fdefinition (find-symbol (string '#:http-fetch) '#:ql-http))
-        (lambda (url &rest rest)
-          (let ((ql:*proxy-url*
-                  (if (eql (search #1="qlot://localhost/" url
-                                   :end2 (length #1#))
-                           0)
-                      nil
-                      ql:*proxy-url*)))
-            (apply #'orig-http-fetch url rest)))))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (unless (fboundp 'orig-http-fetch)
+    ;; dummy for suppress style warning
+    (defun orig-http-fetch (&rest args)
+      (declare (ignore args)))
+    (setf (symbol-function 'orig-http-fetch) (fdefinition
+                                               (find-symbol (string '#:http-fetch) '#:ql-http)))
+    ;; do not use proxy if connect localhost
+    (setf (fdefinition (find-symbol (string '#:http-fetch) '#:ql-http))
+          (lambda (url &rest rest)
+            (let ((ql:*proxy-url*
+                    (if (eql (search #1="qlot://localhost/" url
+                                     :end2 (length #1#))
+                             0)
+                        nil
+                        ql:*proxy-url*)))
+              (apply #'orig-http-fetch url rest))))))
