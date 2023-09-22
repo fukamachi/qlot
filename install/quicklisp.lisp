0424ee593d3df4b77974837fb1fe0f12bfeecd3f
Eitaro Fukamachi <e.arrows@gmail.com>
2023-09-22 15:51:25 +0000

    Delete unnecessary with-tmp-directory.

--- install/quicklisp.lisp
+++ install/quicklisp.lisp
@@ -6,21 +6,18 @@
                 #:*proxy*)
   (:import-from #:qlot/utils/shell
                 #:run-lisp)
-  (:import-from #:qlot/utils/tmp
-                #:with-tmp-directory)
   (:export #:install-quicklisp))
 (in-package #:qlot/install/quicklisp)
 
 (defun install-quicklisp (path)
   (message "Installing Quicklisp to ~A ..." path)
-  (with-tmp-directory (tmp-dir)
-    (let ((quicklisp-file (asdf:system-relative-pathname :qlot #P"quicklisp/quicklisp-installer.lisp")))
-      (run-lisp (list
-                  `(let ((*standard-output* (make-broadcast-stream)))
-                     (load ,quicklisp-file))
-                  "(setf quicklisp-quickstart:*after-initial-setup-message* \"\")"
-                  (format nil "(let ((*standard-output* (make-broadcast-stream)) (*trace-output* (make-broadcast-stream))) (quicklisp-quickstart:install :path #P\"~A\"~@[ :proxy \"~A\"~]))"
-                          path
-                          *proxy*))
-                :without-quicklisp t)
-      t)))
+  (let ((quicklisp-file (asdf:system-relative-pathname :qlot #P"quicklisp/quicklisp-installer.lisp")))
+    (run-lisp (list
+               `(let ((*standard-output* (make-broadcast-stream)))
+                  (load ,quicklisp-file))
+               "(setf quicklisp-quickstart:*after-initial-setup-message* \"\")"
+               (format nil "(let ((*standard-output* (make-broadcast-stream)) (*trace-output* (make-broadcast-stream))) (quicklisp-quickstart:install :path #P\"~A\"~@[ :proxy \"~A\"~]))"
+                       path
+                       *proxy*))
+              :without-quicklisp t)
+    t))
