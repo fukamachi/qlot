8729eb029645d087c14b742b813b5b1637c1d5e5
Eitaro Fukamachi <e.arrows@gmail.com>
2023-09-22 01:44:12 +0000

    Bundle Quicklisp installer.

--- install/quicklisp.lisp
+++ install/quicklisp.lisp
@@ -4,31 +4,17 @@
                 #:message)
   (:import-from #:qlot/proxy
                 #:*proxy*)
-  (:import-from #:qlot/utils
-                #:generate-random-string)
   (:import-from #:qlot/utils/shell
                 #:run-lisp)
   (:import-from #:qlot/utils/tmp
                 #:with-tmp-directory)
-  (:import-from #:qlot/utils/http
-                #:http-fetch)
   (:export #:install-quicklisp))
 (in-package #:qlot/install/quicklisp)
-
-(defun fetch-installer (to)
-  (let ((quicklisp-file (if (uiop:directory-pathname-p to)
-                            (merge-pathnames (format nil "quicklisp-~A.lisp"
-                                                     (generate-random-string))
-                                             to)
-                            to)))
-    (http-fetch "http://beta.quicklisp.org/quicklisp.lisp"
-                quicklisp-file)
-    quicklisp-file))
 
 (defun install-quicklisp (path)
   (message "Installing Quicklisp to ~A ..." path)
   (with-tmp-directory (tmp-dir)
-    (let ((quicklisp-file (fetch-installer tmp-dir)))
+    (let ((quicklisp-file (asdf:system-relative-pathname :qlot #P"quicklisp/quicklisp-installer.lisp")))
       (run-lisp (list
                   `(let ((*standard-output* (make-broadcast-stream)))
                      (load ,quicklisp-file))
