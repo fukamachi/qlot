2ce4e7d4528c89da7606e59f75ee701c8e9987d1
Eitaro Fukamachi <e.arrows@gmail.com>
2023-09-23 01:53:45 +0000

    Fix local-init/qlot-setup.

--- local-init/qlot-setup.lisp
+++ local-init/qlot-setup.lisp
@@ -1,18 +1,20 @@
+
 (defpackage #:qlot/local-init/qlot-setup
   (:use #:cl))
 (in-package #:qlot/local-init/qlot-setup)
 
 (defun setup-source-registry ()
   #+ros.init (setf roswell:*local-project-directories* nil)
-  (let ((source-registry (ql-setup:qmerge "source-registry.conf")))
+  (let* ((source-registry (ql-setup:qmerge "source-registry.conf"))
+         (local-source-registry-form
+           (and (uiop:file-exists-p source-registry)
+                (uiop:read-file-form source-registry))))
     (asdf:initialize-source-registry
-     (or
-      (append
-       (and (uiop:file-exists-p source-registry)
-            (uiop:read-file-form source-registry))
-       `((:tree ,(probe-file (merge-pathnames #P"../" ql:*quicklisp-home*)))))
-      `(:source-registry :ignore-inherited-configuration
-        (:tree ,(probe-file (merge-pathnames #P"../" ql:*quicklisp-home*)))
-        (:also-exclude ".qlot"))))))
+     (if local-source-registry-form
+         (append local-source-registry-form
+                 `((:tree ,(probe-file (merge-pathnames #P"../" ql:*quicklisp-home*)))))
+         `(:source-registry :ignore-inherited-configuration
+           (:tree ,(probe-file (merge-pathnames #P"../" ql:*quicklisp-home*)))
+           (:also-exclude ".qlot"))))))
 
 (setup-source-registry)
