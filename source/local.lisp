58c7153e8c56096255b647931eef39f5b6736cea
Eitaro Fukamachi <e.arrows@gmail.com>
2023-09-23 00:47:51 +0000

    Delete unnecessary :version arg for source-local.

--- source/local.lisp
+++ source/local.lisp
@@ -1,6 +1,8 @@
 (defpackage #:qlot/source/local
   (:use #:cl
         #:qlot/source/base)
+  (:import-from #:qlot/source/base
+                #:initargs)
   (:import-from #:qlot/errors
                 #:invalid-definition)
   (:export #:source-local
@@ -25,3 +27,17 @@
       (error 'invalid-definition
              :source :local
              :usage "local <project name> <directory path>"))))
+
+(defmethod initialize-instance :after ((source source-local) &rest initargs)
+  (setf (slot-value source 'initargs)
+        (loop for (k v) on initargs by #'cddr
+              unless (eq k :version)
+              append (list k v))))
+
+(defmethod freeze-source ((source source-local))
+  (let ((source-params (call-next-method)))
+    (cons (first source-params)
+          ;; Delete unnecessary 'version'
+          (loop for (k v) on (rest source-params) by #'cddr
+                unless (eq k :version)
+                append (list k v)))))
