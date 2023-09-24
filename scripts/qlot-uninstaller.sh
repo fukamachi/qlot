440c3d750c2f8ad3e3badab562092546264a7ebc
Eitaro Fukamachi <e.arrows@gmail.com>
2023-09-24 11:58:04 +0000

    Add an automatic uninstaller.

---
+++ scripts/qlot-uninstaller.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+QLOT_HOME=${QLOT_HOME:-~/.qlot}
+
+rm -rf "$QLOT_HOME"
+
+if [ `id -u` == "0" ]; then
+  rm /usr/local/bin/qlot
+fi
+
+if [ "$(which sbcl)" != "" ]; then
+  lisp="sbcl"
+elif [ "$(which ros)" != "" ]; then
+  lisp="ros without-roswell=t -L sbcl-bin run --"
+else
+  exit
+fi
+
+systems_directory() {
+  $lisp --noinform --no-sysinit --no-userinit --non-interactive \
+    --eval '(require :asdf)' --eval '(princ (uiop:native-namestring (uiop:xdg-data-home #P"common-lisp/systems/")))'
+}
+
+registry_dir=$(systems_directory)
+rm -f "${registry_dir}qlot.asd"
