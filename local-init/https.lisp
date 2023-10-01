063c11c65ed63518248da7ee7d99e28bc1d32c33
Eitaro Fukamachi <e.arrows@gmail.com>
2023-10-01 10:48:52 +0000

    Add local-init/https to download via HTTPS without Qlot.

---
+++ local-init/https.lisp
@@ -0,0 +1,43 @@
+(defpackage #:qlot/local-init/https
+  (:use #:cl))
+(in-package #:qlot/local-init/https)
+
+(defun https-of (url)
+  (if (and (stringp url)
+           (<= 7 (length url))
+           (search "http://" url :end2 7))
+      (format nil "https://~A" (subseq url 7))
+      url))
+
+(defun which (cmd)
+  (handler-case
+      (string-right-trim '(#\Newline)
+                         (with-output-to-string (s)
+                           (uiop:run-program `("which" ,cmd)
+                                             :output s)))
+    (uiop/run-program:subprocess-error ()
+      nil)))
+
+(defun curl-fetch (url file &rest args)
+  (declare (ignore args))
+  (let ((url (https-of url)))
+    (format t "~&; Fetching '~A'.~%" url)
+    (let ((now (get-internal-real-time)))
+      (uiop:run-program (list "curl" "-sSL" url "-o" (uiop:native-namestring file))
+                        :error-output :interactive)
+      (format t "~&; Done '~A' (~$KB) in ~A seconds.~%"
+              (file-namestring file)
+              (/ (ql-util:file-size file) 1024)
+              (coerce
+               (/ (- (get-internal-real-time) now)
+                  internal-time-units-per-second)
+               'float)))))
+
+(defun add-to-fetch-scheme-functions ()
+  (when (which "curl")
+    (setf ql-http:*fetch-scheme-functions*
+          (append '(("https" . curl-fetch)
+                    ("http" . curl-fetch))
+                  ql-http:*fetch-scheme-functions*))))
+
+(add-to-fetch-scheme-functions)
