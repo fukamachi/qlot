2b5826750861289284d5bf0d4b99f8373536c7df
Eitaro Fukamachi <e.arrows@gmail.com>
2023-10-01 12:13:06 +0000

    Fallback to curl.

--- local-init/https.lisp
+++ local-init/https.lisp
@@ -21,13 +21,42 @@
                     :output :interactive
                     :error-output :interactive))
 
+(defun which (cmd)
+  (handler-case
+      (string-right-trim '(#\Newline)
+                         (with-output-to-string (s)
+                           (uiop:run-program `("which" ,cmd)
+                                             :output s)))
+    (uiop/run-program:subprocess-error ()
+      nil)))
+
+(defun curl-fetch (url file &rest args)
+  (declare (ignore args))
+  (let ((url (https-of url)))
+    (format t "~&; Fetching '~A'.~%" url)
+    (let ((now (get-internal-real-time)))
+      (uiop:run-program (list "curl" "-sSL" url "-o" (uiop:native-namestring file))
+                        :error-output :interactive)
+      (format t "~&; Done '~A' (~$KB) in ~A seconds.~%"
+              (file-namestring file)
+              (/ (ql-util:file-size file) 1024)
+              (coerce
+               (/ (- (get-internal-real-time) now)
+                  internal-time-units-per-second)
+               'float)))))
+
 (defun add-to-fetch-scheme-functions ()
-  (when (and (uiop:file-exists-p *fetch-script*)
-             (uiop:file-exists-p
-              (asdf:system-relative-pathname :qlot #P".qlot/setup.lisp")))
-    (setf ql-http:*fetch-scheme-functions*
-          (append '(("https" . run-fetch)
-                    ("http" . run-fetch))
-                  ql-http:*fetch-scheme-functions*))))
+  (let ((fn (cond
+              ((and (uiop:file-exists-p *fetch-script*)
+                    (uiop:file-exists-p
+                     (asdf:system-relative-pathname :qlot #P".qlot/setup.lisp")))
+               'run-fetch)
+              ((which "curl")
+               'curl-fetch))))
+    (when fn
+      (setf ql-http:*fetch-scheme-functions*
+            (append `(("https" . ,fn)
+                      ("http" . ,fn))
+                    ql-http:*fetch-scheme-functions*)))))
 
 (add-to-fetch-scheme-functions)
