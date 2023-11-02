7e1bac31972f933b691f2a89c36e153e70ae2d48
Eitaro Fukamachi <e.arrows@gmail.com>
2023-11-02 11:29:01 +0000

    Fix qlot/proxy.

--- proxy.lisp
+++ proxy.lisp
@@ -19,23 +19,28 @@
   ;; For some reason, a separate function definition
   ;; works as expected.
   (defun qlot-http-fetch (url &rest rest)
-    (let ((ql:*proxy-url*
-            (if (eql (search #1="qlot://localhost/" url
-                             :end2 (length #1#))
-                     0)
-                nil
-                ql:*proxy-url*)))
-      (apply #'orig-http-fetch url rest))))
+    (if (find :quicklisp *features*)
+        (progv (list (intern (string '#:*proxy-utl*) '#:ql))
+            (list
+             ;; do not use proxy if connect localhost
+             (if (eql (search #1="qlot://localhost/" url
+                              :end2 (length #1#))
+                      0)
+                 nil
+                 (symbol-value (intern (string '#:*proxy-utl*) '#:ql))))
+          (apply #'orig-http-fetch url rest))
+        (apply #'orig-http-fetch url rest))))
 
 #+quicklisp
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (unless (fboundp 'orig-http-fetch)
     ;; dummy for suppress style warning
     (defun orig-http-fetch (&rest args)
-      (declare (ignore args)))
-    (setf (symbol-function 'orig-http-fetch)
-          (fdefinition
-           (find-symbol (string '#:http-fetch) '#:ql-http)))
-    ;; do not use proxy if connect localhost
-    (setf (fdefinition (find-symbol (string '#:http-fetch) '#:ql-http))
-          #'qlot-http-fetch)))
+      (declare (ignore args))
+      (error "A dummy function is called"))
+    (when (find-package '#:ql-http)
+      (setf (symbol-function 'orig-http-fetch)
+            (fdefinition
+             (find-symbol (string '#:http-fetch) '#:ql-http)))
+      (setf (fdefinition (find-symbol (string '#:http-fetch) '#:ql-http))
+            #'qlot-http-fetch))))
