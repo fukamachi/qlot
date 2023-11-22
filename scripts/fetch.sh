562d2d8950c0e87b4e7023aedcbeb360628efc16
Eitaro Fukamachi <e.arrows@gmail.com>
2023-11-22 00:58:26 +0000

    Tweak fetching script.

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -18,8 +18,18 @@ else
   exit 1
 fi
 
+if [ -f "$QLOT_SOURCE_DIR/.bundle-libs/bundle.lisp" ]; then
+  setup_file="$QLOT_SOURCE_DIR/.bundle-libs/bundle.lisp"
+elif [ -f "$QLOT_SOURCE_DIR/.qlot/setup.lisp" ]; then
+  setup_file="$QLOT_SOURCE_DIR/.qlot/setup.lisp"
+else
+  echo "Qlot is not setup yet." >&2
+  echo "Run '$QLOT_SOURCE_DIR/scripts/setup.sh' first." >&2
+  exit 1
+fi
+
 exec $lisp --noinform --no-sysinit --no-userinit --non-interactive \
-  --load $QLOT_SOURCE_DIR/.qlot/setup.lisp \
+  --load "$setup_file" \
   --eval "(asdf:load-asd #P\"$QLOT_SOURCE_DIR/qlot.asd\")" \
-  --eval '(ql:quickload :qlot/fetch :silent t)' \
+  --eval '(let ((*standard-output* (make-broadcast-stream)) (*trace-output* (make-broadcast-stream))) (asdf:load-system :qlot/fetch))' \
   --eval '(qlot/fetch::main)' -- "$@"
