4b6d34c5a5266db85e55f9b061ddc2cae51b64df
Eitaro Fukamachi <e.arrows@gmail.com>
2023-12-09 15:52:09 +0000

    Revert "Delete run-lisp and launch-lisp."
    This reverts commit 9059bd9a08b9da6651e8f16dd8c4a3289095040f.

--- src/utils/shell.lisp
+++ src/utils/shell.lisp
@@ -5,8 +5,14 @@
                 #:debug-log)
   (:export #:safety-shell-command
            #:shell-command-error
-           #:shell-command-error-output))
+           #:shell-command-error-output
+           #:run-lisp
+           #:launch-lisp
+           #:*qlot-source-directory*))
 (in-package #:qlot/utils/shell)
+
+(defparameter *qlot-source-directory*
+  (asdf:system-source-directory :qlot))
 
 (define-condition shell-command-error (simple-error)
   ((command :type cons
@@ -41,3 +47,150 @@
                :command (cons program args)
                :code (uiop/run-program:subprocess-error-code e)
                :stderr (uiop:slurp-stream-string stderr))))))
+
+(defun safety-background-command (program args &key input output)
+  (setf args (mapcar #'princ-to-string args))
+  (debug-log "Running a background command: ~A~{ ~S~}" program args)
+  (uiop:launch-program (cons program args)
+                       :input input
+                       :output output
+                       :error-output :stream))
+
+(defvar *current-lisp-path*
+  (or #+ccl (car ccl:*command-line-argument-list*)
+      #+sbcl (car sb-ext:*posix-argv*)
+      #+allegro (car (system:command-line-arguments))
+      #+clisp "clisp"
+      #+cmu (car ext:*command-line-strings*)
+      #+ecl (car (si:command-args))
+      #+abcl "java"))
+
+(defvar *eval-option*
+  (or
+    #+ros.init "-e"
+    #+ccl "--eval"
+    #+sbcl "--eval"
+    #+allegro "-e"
+    #+clisp "-x"
+    #+cmu "-eval"
+    #+ecl "-eval"
+    #+abcl "--eval"))
+
+(defun str (form)
+  #+sbcl (declare (sb-ext:muffle-conditions sb-ext:code-deletion-note))
+  (let ((*package* (find-package :cl-user)))
+    (if (stringp form)
+        form
+        (let ((*print-case* :downcase) (*print-pretty* nil))
+          (prin1-to-string form)))))
+
+(defun -e (form)
+  (list *eval-option* (str form)))
+
+(defun default-args ()
+  (append
+    (-e "(require 'asdf)")
+    (-e
+      `(setf *debugger-hook*
+             (lambda (cl-user::c cl-user::parent)
+               (declare (ignore cl-user::parent))
+               (princ cl-user::c *error-output*)
+               (fresh-line *error-output*)
+               ,@(and *debug*
+                      '((uiop:print-backtrace :condition cl-user::c)))
+               (uiop:quit -1))))))
+
+(defun build-command-args (forms &key quicklisp-home systems source-registry)
+  (append
+   (default-args)
+
+   (when source-registry
+     (-e `(push ,source-registry asdf:*central-registry*)))
+
+   (-e '(setf asdf::*default-source-registries*
+         (quote (asdf::environment-source-registry
+                 asdf::system-source-registry
+                 asdf::system-source-registry-directory))))
+
+   (let* ((quicklisp-home (or quicklisp-home
+                              (and (find :quicklisp *features*)
+                                   (symbol-value (intern (string '#:*quicklisp-home*) '#:ql)))))
+          (setup
+            (or (probe-file (asdf:system-relative-pathname :qlot #P".bundle-libs/bundle.lisp"))
+                (and quicklisp-home
+                     (merge-pathnames #P"setup.lisp" quicklisp-home)))))
+     (when setup
+       (-e `(load ,setup))))
+
+   (loop for system in systems
+         append (-e
+                 `(if (find :quicklisp *features*)
+                      (uiop:symbol-call :ql :quickload ,system :silent t)
+                      (let ((*standard-output* (make-broadcast-stream))
+                            (*error-output* (make-broadcast-stream))
+                            (*trace-output* (make-broadcast-stream)))
+                        (asdf:load-system ,system)))))
+
+   (loop for form in forms
+         append (-e
+                 (if (pathnamep form)
+                     `(load ,form)
+                     form)))))
+
+#+ros.init
+(defun precommand-options ()
+  '("+Q"))
+
+#-ros.init
+(defun precommand-options ()
+  #+abcl `("-jar" ,(uiop:native-namestring
+                    (first (pathname-device ext:*lisp-home*))))
+
+  #+ccl '("--no-init" "--quiet" "--batch")
+  #+sbcl '("--noinform" "--no-sysinit" "--no-userinit" "--non-interactive")
+  #+allegro '("--qq")
+  #+clisp '("-norc" "--quiet" "--silent" "-on-error" "exit")
+  #+cmu '("-noinit")
+  #+ecl '("-norc")
+  #+abcl '("--noinform" "--noinit"))
+
+#+ros.init
+(defun postcommand-options () nil)
+
+#-ros.init
+(defun postcommand-options ()
+  (-e
+   (quote
+    #+ccl (ccl:quit)
+    #+sbcl (sb-ext:exit)
+    #+allegro (excl:exit :quiet t)
+    #+clisp (ext:quit)
+    #+cmucl (unix:unix-exit)
+    #+ecl (ext:quit)
+    #+abcl (ext:quit)
+    #-(or ccl sbcl allegro clisp cmucl ecl abcl) (cl-user::quit))))
+
+(defun command-options (forms args)
+  (append
+   (precommand-options)
+   (apply #'build-command-args forms args)
+   (postcommand-options)))
+
+(defun launch-lisp (forms &rest args &key systems source-registry)
+  (declare (ignore systems source-registry))
+  (safety-background-command
+   #-ros.init *current-lisp-path*
+   #+ros.init (or (ros:opt "wargv0")
+                  (ros:opt "argv0"))
+   (command-options forms args)
+   :input :stream
+   :output :stream))
+
+(defun run-lisp (forms &rest args &key quicklisp-home systems source-registry (output :interactive))
+  (declare (ignore quicklisp-home systems source-registry))
+  (remf args :output)
+  (safety-shell-command #-ros.init *current-lisp-path*
+                        #+ros.init (or (ros:opt "wargv0")
+                                       (ros:opt "argv0"))
+                        (command-options forms args)
+                        :output output))
